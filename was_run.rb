9407fbce6ce65edc23888d306395f122624f9819
Ken Mayer <ken@bitwrangler.com>
2014-05-04 15:30:49 -0700

    Refactor runner into TestCase class
    - Separate responsibilites

--- was_run.rb
+++ was_run.rb
@@ -1,14 +1,17 @@
-class WasRun
-  attr_accessor :wasRun
+class TestCase
   attr_reader :test_method
-  
-  def initialize(test)
-    @test_method = test
+
+  def initialize(test_method)
+    @test_method = test_method
   end
 
   def run
     self.public_send(@test_method)
   end
+end
+
+class WasRun < TestCase
+  attr_accessor :wasRun
 
   def testMethod
     self.wasRun = true
